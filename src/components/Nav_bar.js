400b97c1575fcb92ecbd6761a9ceb0daa96f81f4
Divyansh Gupta <123203281+divyansh12git@users.noreply.github.com>
2023-05-21 02:11:24 +0530

    4th

--- src/components/Nav_bar.js
+++ src/components/Nav_bar.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 export default function NaviBar(props) {
   return (
@@ -13,7 +12,7 @@ export default function NaviBar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/about">{props.AboutText}</a>
+          <a className="nav-link active" aria-current="page" href="">{props.AboutText}</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/">Link</a>
